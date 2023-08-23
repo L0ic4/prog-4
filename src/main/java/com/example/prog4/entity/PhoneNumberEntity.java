41825a945072ac724731d9b6d9ab8d649d5ed8ed
NiceGuyMe <loica.etudiant@gmail.com>
2023-08-23 17:01:32 +0300

    chore: add phone number validation

--- src/main/java/com/example/prog4/entity/PhoneNumberEntity.java
+++ src/main/java/com/example/prog4/entity/PhoneNumberEntity.java
@@ -1,6 +1,5 @@
 package com.example.prog4.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,7 +7,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,13 +21,19 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(uniqueConstraints = { @UniqueConstraint(name = "UniquePhoneNumbers", columnNames = { "phoneNumber", "countryCode" }) })
+@Table(uniqueConstraints = {
+    @UniqueConstraint(name = "UniquePhoneNumbers", columnNames = {"phoneNumber", "countryCode"})})
 public class PhoneNumberEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
-  private String phoneNumber;
+  @NotBlank(message = "Country code is required")
+//  @Pattern(regexp= "^[+][0-9]{1,3}$",message = "Invalid country code")
   private String countryCode;
+
+  @NotBlank(message = "Phone number is required")
+//  @Pattern(regexp = "^[0-9]{9,15}$", message = "Invalid phone number")
+  private String phoneNumber;
   @ManyToOne
   private EmployeeEntity employee;
 }
